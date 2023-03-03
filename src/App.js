26e122c7c2b259c048cabcb3ea4832a85a0674a8
huyhoangm2 <hoangken760@gmail.com>
2023-03-04 00:09:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Tên của bạn phải có ít nhất 2 ký tự")
+    .max(50, "Tên của bạn không được dài hơn 50 ký tự")
+    .required("Vui lòng nhập tên người dùng"),
+  email: Yup.string()
+    .email("Email không hợp lệ")
+    .required("Vui lòng nhập địa chỉ email"),
+  phone: Yup.string()
+    .matches(/^[0-9]{10}$/, "Vui lòng nhập số điện thoại hợp lệ")
+    .required("Vui lòng nhập số điện thoại"),
+  password: Yup.string()
+    .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
+    .max(50, "Mật khẩu không được dài hơn 50 ký tự")
+    .required("Vui lòng nhập mật khẩu"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Mật khẩu nhập lại không trùng khớp")
+    .required("Bạn cần nhập lại mật khẩu"),
+});
+
+const SignupForm = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        phone: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="name">Tên người dùng</label>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="phone">Số điện thoại</label>
+            <Field type="text" name="phone" />
+            <ErrorMessage name="phone" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="password">Mật khẩu</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Nhập lại mật khẩu</label>
+            <Field type="password" name="confirmPassword" />
+            <ErrorMessage
+              name="confirmPassword"
+              component="div"
+              className="error"
+            />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Đăng ký
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
-}
+};
 
-export default App;
+export default SignupForm
